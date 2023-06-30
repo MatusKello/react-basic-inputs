df35b497c800c4522b0eaffe9cbd36c10cb21557
Matus Kello <m.kello1992@gmail.com>
2023-06-30 17:01:28 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+//import Input1 from './components/Input1';
+import Input2 from './components/Input2';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Input2 />
     </div>
   );
-}
+};
 
 export default App;
