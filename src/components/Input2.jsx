5c896e0c991ab945d91c60e71d3ecafee6fdd3c6
Matus Kello <m.kello1992@gmail.com>
2023-07-01 11:43:09 +0200

    reducing useState, refactoring

--- src/components/Input2.jsx
+++ src/components/Input2.jsx
@@ -1,48 +1,75 @@
 import { useState } from 'react';
 
 const Input2 = () => {
-  const [fullName, setFullName] = useState('');
+  /*   const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
+  const [age, setAge] = useState(''); */
+
+  const [oneUser, setOneUser] = useState({ fullName: '', email: '', age: '' });
   const [users, setUsers] = useState([]);
 
+  const handleFormChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    //!dynamic object
+    setOneUser({ ...oneUser, [name]: value });
+  };
+
+  //!refactoring
   const formSubmit = (e) => {
     e.preventDefault();
-
-    if (fullName && email) {
-      const oneUser = { fullName: fullName, email: email };
+    if (oneUser.fullName && oneUser.email && oneUser.age) {
+      /* const newUser = {
+        fullName: oneUser.fullName,
+        email: oneUser.email,
+        age: oneUser.age,
+      }; */
+      const newUser = { ...oneUser, id: new Date().getTime() };
       setUsers((users) => {
-        return [...users, oneUser];
+        return [...users, newUser];
       });
+      setOneUser({ fullName: '', email: '', age: '' });
     } else {
       console.log('something wrong');
     }
-    setEmail('');
-    setFullName('');
+    /*     setFullName("")
+    setEmail("")
+    setAge("")  */
   };
 
   return (
     <article>
       <form onSubmit={formSubmit}>
         <input
-          value={fullName}
-          onChange={(event) => setFullName(event.target.value)}
+          value={oneUser.fullName}
+          name='fullName'
+          onChange={handleFormChange}
           type='text'
           placeholder='Name'
         />
         <input
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          value={oneUser.email}
+          name='email'
+          onChange={handleFormChange}
           type='email'
           placeholder='Email'
         />
+        <input
+          value={oneUser.age}
+          name='age'
+          onChange={handleFormChange}
+          type='text'
+          placeholder='Age'
+        />
         <input type='submit' />
       </form>
-      {users.map((oneUser, index) => {
-        const { fullName, email } = oneUser;
+      {users.map((oneUser) => {
+        const { id, fullName, email, age } = oneUser;
         return (
-          <div className='item' key={index}>
+          <div className='item' key={id}>
             <h4>{fullName}</h4>
             <p>{email}</p>
+            <p>{age}</p>
           </div>
         );
       })}
